9e07000b3de02f2b488e6577eb60016083feaa44
Fernando Aguilar <aguilarf47@gmail.com>
2019-05-04 17:16:58 -0700

    fix

--- myapi/api/views.py
+++ myapi/api/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 
-from myapi.api.resources import UserResource, UserList, HomePage
+from myapi.api.resources import UserResource, UserList, HomePage, HomePage2
 
 
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
@@ -11,4 +11,4 @@ api = Api(blueprint)
 api.add_resource(UserResource, '/users/<int:user_id>')
 api.add_resource(UserList, '/users')
 api.add_resource(HomePage, '/home')
-api.add_resource(HomePage, '/home2')
+api.add_resource(HomePage2, '/home2')
